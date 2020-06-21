dcfd273b54cf8d9e97a7550448ffff0a8daa0738
abledenthusiast <pitmana@xavier.edu>
2020-06-20 20:16:57 -0700

    add scheduled execution

--- src/extractor.py
+++ src/extractor.py
@@ -5,10 +5,15 @@ import schedule
 
 client = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database='telegraf')
 def extract():
-    print(time.now())
-    result = select_prev_metrics()
-    if result is not None:
-        post_to_service(result)
+    try:
+        print(time.time())
+        
+        result = select_prev_metrics()
+        if result is not None:
+            post_to_service(result)
+
+    except Exception as exc:
+        print(exc)
 
 def select_prev_metrics():
     query = 'select * FROM ping WHERE time > now() - 24h GROUP BY url;'
@@ -16,8 +21,6 @@ def select_prev_metrics():
     # nested tuples are used here, so: list of metrics -> tuple of (ping, 'url' : url)
     result_dict = { result_items[i][0][1]['url'] : list(result_items[i][1]) for i in range(0, len(result_items))}
     return result_dict
-
-
 
 def post_to_service(metrics):
     for url,stats in metrics.items():
@@ -27,7 +30,7 @@ def post_to_service(metrics):
 
 if __name__ == '__main__':
     print('starting')
-    schedule.every().hour.do(extract)
+    schedule.every(12).hours.do(extract)
     while True:
         schedule.run_pending()
-        time.sleep(600) # wait ten minutes
+        time.sleep(300) # wait five minutes
